415f09b717da708bb644f4fc0b355b28b85541e7
Joel Hoil <yoelvann@gmail.com>
2022-04-09 23:23:47 -0500

    Finished  exercises

---
+++ weekly_mission_1/example9/weekly_mission_1/pokemon.js
@@ -0,0 +1,16 @@
+
+class Pokemon{
+    constructor(name){
+        this.name = name
+    }
+
+    sayHello(){
+        console.log(`Mi Pokémon ${ this.name } te saluda!`)
+    }
+
+    sayMessage(message){
+        console.log(`Mi Pokémon ${ this.name } dice: ${ message }`)
+    }
+}
+
+module.exports = Pokemon
